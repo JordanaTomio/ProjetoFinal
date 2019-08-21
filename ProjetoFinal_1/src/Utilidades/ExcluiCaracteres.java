bfeaf1337f776715f1147e807756eaa580144a31
ivo <ivopaulo.puehler@gmail.com>
2019-08-21 16:07:40 -0300

    Atualizaçao de bloqueio de Text

---
+++ ProjetoFinal_1/src/Utilidades/ExcluiCaracteres.java
@@ -0,0 +1,34 @@
+package Utilidades;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+	// bloqueia caracteres
+
+public class ExcluiCaracteres extends PlainDocument{
+	
+    private CharSequence chars;
+
+    public ExcluiCaracteres(CharSequence lockedChars) {
+        chars = lockedChars;
+    }
+
+    @Override
+    public void insertString(int offset, String str, AttributeSet attr)
+            throws BadLocationException{
+
+        boolean teste = false;
+        if (str == null) super.insertString(offset, str, attr);
+        int length = chars.length();
+        for(int i = 0; i < length; i++){
+            if (str.contains(""+chars.charAt(i)+"")){
+                return;
+            }
+            else{
+                teste = true;
+            }
+        }
+        if(teste == true) super.insertString(offset, str, attr);
+    }
+}
