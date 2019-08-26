660d82d58074430762a4741cdef9ce84ab416990
papaleguas9693 <53578635+papaleguas9693@users.noreply.github.com>
2019-08-26 14:46:19 -0300

    add metodo Adotar bixim

--- ProjetoFinal_1/src/DAO/AnimalDAO.java
+++ ProjetoFinal_1/src/DAO/AnimalDAO.java
@@ -113,8 +113,8 @@ public class AnimalDAO {
 
 	public static ResultSet BuscaAnimal(int one) {
 
-		String q = "SELECT  CD_Animal, NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal "
-				+ " WHERE CD_Animal > " + one + " ORDER BY CD_Animal";
+		String q = "SELECT  CD_Animal, NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal, AD_Animal FROM animal "
+				+ " WHERE AD_Animal = 0 and CD_Animal > " + one + " ORDER BY CD_Animal";
 
 		System.out.println(q);
 		try {
@@ -134,14 +134,16 @@ public class AnimalDAO {
 		MySQLConfiguration c = new MySQLConfiguration();
 
 		try {
-			Statement cs = c.conn.createStatement();
+			Statement ps = c.conn.createStatement();
 
 			String q = "";
-			q += "delete from animal where NM_Animal = ";
-			q += "'" + nomePet + "'";
+			String x = "";
 			
+			x += "'" + nomePet + "'";
+			q += " UPDATE animal SET AD_Animal = 1 WHERE NM_Animal = "+ x;
+					
 			System.out.println(q);
-			cs.executeUpdate(q);
+			 ps.executeUpdate(q);
 
 
 		} catch (Exception ex) {
@@ -153,6 +155,7 @@ public class AnimalDAO {
 					MySQLConfiguration.conn.close();
 				} catch (Exception ex) {
 					System.out.println("Error in connection");
+					
 				}
 			}
 		}
