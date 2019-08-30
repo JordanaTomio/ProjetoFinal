0b78d12bc5bf0727274d90421b4408b720719ac6
Ivo-loop <53790303+Ivo-loop@users.noreply.github.com>
2019-08-30 16:00:58 -0300

    Correçao bug na consulta adotar

--- ProjetoFinal_1/src/DAO/AnimalDAO.java
+++ ProjetoFinal_1/src/DAO/AnimalDAO.java
@@ -9,110 +9,8 @@ import Frames.Main;
 import br.com.idog.Configuration.MySQLConfiguration;
 
 public class AnimalDAO {
-	public static void selectAll(String nome) {
-		MySQLConfiguration c = new MySQLConfiguration();
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where NM_Animal ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + nome + "%");
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return;
-		}
-	}
 
-	public static void selectRaca(String nome) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where NM_Animal ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + nome + "%");
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return;
-		}
-	}
-
-	public static ResultSet selectByRaca(String raca) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where RC_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + raca + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet selectByAno(String ano) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where ANO_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + ano + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet selectByTamanho(String tamanho) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where ANO_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + tamanho + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet selectBySexo(String sexo) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where SX_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + sexo + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet selectByTipo(String tipo) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where TP_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + tipo + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet selectByVacinas(String vacinas) {
-		String q = "SELECT NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal where VC_Animal like ?";
-		try {
-			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
-			ps.setString(1, "%" + vacinas + "%");
-			return ps.executeQuery();
-		} catch (SQLException e) {
-			System.err.println("Animal isn't found.");
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	public static ResultSet BuscaAnimal(int one) {
+	public static ResultSet BuscaAnimalMaior(String one) {
 
 		String q = "SELECT  CD_Animal, NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal, AD_Animal FROM animal "
 				+ " WHERE AD_Animal = 0 and CD_Animal > " + one + " ORDER BY CD_Animal";
@@ -130,8 +28,26 @@ public class AnimalDAO {
 		}
 
 	}
+	public static ResultSet BuscaAnimalMenor(String one) {
 
-	public static void AdotarBixim(String nomePet) {
+		String q = "SELECT  CD_Animal, NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal, AD_Animal FROM animal "
+				+ " WHERE AD_Animal = 0 and CD_Animal < " + one + " ORDER BY CD_Animal desc";
+
+		System.out.println(q);
+		try {
+			PreparedStatement ps = MySQLConfiguration.conn.prepareStatement(q);
+			return ps.executeQuery();
+
+		} catch (Exception ex) {
+			System.out.println("Erro no SQL");
+			System.out.println("aqui2");
+			// ex.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static void AdotarAnimal(String nomePet) {
 		MySQLConfiguration c = new MySQLConfiguration();
 
 		try {
@@ -162,9 +78,8 @@ public class AnimalDAO {
 		}
 		
 	}
-	public static ResultSet BuscaAnimaldelete(int one) {
+	public static ResultSet BuscaAnimalUpdate(int one) {
 		one++;
-		//pelo amor de deus nao apaguem essa conexao
 		MySQLConfiguration c = new MySQLConfiguration();
 
 		String q = "SELECT  CD_Animal, NM_Animal, ANO_Animal, TM_Animal, DS_Animal, RC_Animal, TP_Animal, SX_Animal, VC_Animal, CS_Animal FROM animal "
@@ -197,14 +112,14 @@ public class AnimalDAO {
 		}catch (
 
 		Exception ex) {
-			System.err.println("FALHA NA no bang da CONFIRMAÃ‡ÃƒO");
+			System.err.println("FALHA NA no bang da CONFIRMAÃƒâ€¡ÃƒÆ’O");
 			ex.printStackTrace();
 		} finally {
 			if (MySQLConfiguration.conn != null) {
 				try {
 					MySQLConfiguration.conn.close();
 				} catch (Exception ex) {
-					System.err.println("ALGO ERRADO NÃƒO ESTÃ� CERTO!");
+					System.err.println("ALGO ERRADO NÃƒÆ’O ESTÃƒï¿½ CERTO!");
 				}
 			}
 		}
