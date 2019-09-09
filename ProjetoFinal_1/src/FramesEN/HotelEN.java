fa5f8d14cbf92f6af6bf739ca7e4525b44106b33
Jordana Tomio <dannatomio@gmail.com>
2019-09-09 17:15:21 -0300

    Update HotelEN.java

--- ProjetoFinal_1/src/FramesEN/HotelEN.java
+++ ProjetoFinal_1/src/FramesEN/HotelEN.java
@@ -16,6 +16,7 @@ import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -25,6 +26,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.text.MaskFormatter;
 
 import DAO.HotelDAO;
+import Frames.Menu;
+
 import java.awt.Color;
 import java.awt.Toolkit;
 
@@ -38,6 +41,7 @@ public class HotelEN extends JFrame {
 	private JFormattedTextField data;
 	private JFormattedTextField data2;
 	private JTextField txtNome;
+
 	JTextPane txtDs = new JTextPane();
 	int one = -1;
 	private JTextField txtObs;
@@ -93,7 +97,7 @@ public class HotelEN extends JFrame {
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
-		
+
 		JRadioButton gato = new JRadioButton("Cat");
 		buttonGroup.add(gato);
 		gato.setContentAreaFilled(false);
@@ -101,9 +105,9 @@ public class HotelEN extends JFrame {
 		gato.setOpaque(false);
 		gato.setFocusPainted(false);
 		gato.setFont(new Font("Lucida Bright", Font.BOLD, 14));
-		gato.setBounds(177, 223, 63, 23);
+		gato.setBounds(190, 224, 63, 23);
 		contentPane.add(gato);
-		
+
 		JRadioButton cachorro = new JRadioButton("Dog");
 		buttonGroup.add(cachorro);
 		cachorro.setContentAreaFilled(false);
@@ -111,31 +115,44 @@ public class HotelEN extends JFrame {
 		cachorro.setOpaque(false);
 		cachorro.setFocusPainted(false);
 		cachorro.setFont(new Font("Lucida Bright", Font.BOLD, 14));
-		cachorro.setBounds(254, 223, 101, 23);
+		cachorro.setBounds(265, 224, 101, 23);
 		contentPane.add(cachorro);
 
 		JButton btnFinalizar = new JButton("Schedule");
 		btnFinalizar.setForeground(new Color(0, 100, 0));
 		btnFinalizar.setBackground(Color.WHITE);
 		btnFinalizar.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent e) {
 				String nome = txtNomeCliente.getText();
 				String dataComeco = data.getText();
 				String dataFinal = data2.getText();
 				String cuidador = txtNome.getText();
 				String obs = txtObs.getText();
-				
-				if (cachorro.isSelected()){
+				String ds = txtDs.getText();
+				txtDs.setText("");
+
+				if (cachorro.isSelected()) {
 					tipo = "Cachorro";
 					System.out.println("dog");
 				} else if (gato.isSelected()) {
 					tipo = "Gato";
 					System.out.println("cat");
 				}
-				
-				
-				DAO.HotelDAO.Agendar(nome, dataComeco, dataFinal, cuidador, obs, tipo);
-				
+
+				if (!nome.isEmpty() && !dataComeco.isEmpty() && !dataFinal.isEmpty() && !cuidador.isEmpty()
+						&& !tipo.isEmpty()) {
+					Beans.HotelBeans hotel = new Beans.HotelBeans();
+					DAO.HotelDAO.Agendar(nome, dataComeco, dataFinal, cuidador, obs, tipo);
+					DAO.HotelDAO.save(hotel);
+					new Menu().setVisible(true);
+					dispose();
+					JOptionPane.showMessageDialog(null, "Appointment scheduled!");
+				} else {
+					JOptionPane.showMessageDialog(null, "Missing information!");
+				}
+
+				// JOptionPane.showMessageDialog(null, "Data marcada.");
 			}
 		});
 
@@ -148,7 +165,7 @@ public class HotelEN extends JFrame {
 		txtDs.setContentType("");
 		txtDs.setBounds(424, 179, 191, 135);
 		contentPane.add(txtDs);
-		
+
 		txtNome = new JTextField();
 		txtNome.setEditable(false);
 		scrollPane.setViewportView(txtNome);
@@ -187,116 +204,114 @@ public class HotelEN extends JFrame {
 		btnVai.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/3847912-128(2).png")));
 		btnVai.setBounds(523, 325, 89, 51);
 		contentPane.add(btnVai);
-		
-				btnVolta = new JButton("");
-				btnVolta.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						one--;
-						ResultSet rs = HotelDAO.findAll(one);
 
-						try {
-							if (rs.next()) {
-								String nome = rs.getString("NM_Cuidador");
-								String ds = rs.getString("DS_Cuidador");
+		btnVolta = new JButton("");
+		btnVolta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				one--;
+				ResultSet rs = HotelDAO.findAll(one);
 
-								txtNome.setText(nome);
-								txtDs.setText(ds);
-							}
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
+				try {
+					if (rs.next()) {
+						String nome = rs.getString("NM_Cuidador");
+						String ds = rs.getString("DS_Cuidador");
 
+						txtNome.setText(nome);
+						txtDs.setText(ds);
 					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 
-				});
-				btnVolta.setContentAreaFilled(false);
-				btnVolta.setOpaque(false);
-				btnVolta.setFocusPainted(false);
-				btnVolta.setBorderPainted(false);
-				
-						btnVolta.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/3847912-128(1).png")));
-						btnVolta.setBounds(424, 325, 89, 51);
-						contentPane.add(btnVolta);
-		
-				JButton sair = new JButton("");
-				sair.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						MenuEN mnu = new MenuEN();
-						mnu.setVisible(true);
-						dispose();
-					}
-				});
-				sair.setContentAreaFilled(false);
-				sair.setOpaque(false);
-				sair.setFocusPainted(false);
-				sair.setBorderPainted(false);
-				sair.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/3209260-128(1).png")));
-				sair.setBounds(10, 336, 63, 51);
-				contentPane.add(sair);
+			}
+
+		});
+		btnVolta.setContentAreaFilled(false);
+		btnVolta.setOpaque(false);
+		btnVolta.setFocusPainted(false);
+		btnVolta.setBorderPainted(false);
+
+		btnVolta.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/3847912-128(1).png")));
+		btnVolta.setBounds(424, 325, 89, 51);
+		contentPane.add(btnVolta);
+
+		JButton sair = new JButton("");
+		sair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Menu mnu = new Menu();
+				mnu.setVisible(true);
+				dispose();
+			}
+		});
+		sair.setContentAreaFilled(false);
+		sair.setOpaque(false);
+		sair.setFocusPainted(false);
+		sair.setBorderPainted(false);
+		sair.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/3209260-128(1).png")));
+		sair.setBounds(10, 336, 63, 51);
+		contentPane.add(sair);
 
 		data2 = new JFormattedTextField(mascaras);
 		data2.setFont(new Font("Lucida Bright", Font.PLAIN, 16));
-		data2.setBounds(318, 183, 54, 19);
+		data2.setBounds(312, 183, 54, 19);
 		contentPane.add(data2);
-		
+
 		data = new JFormattedTextField(mascaras);
 		data.setFont(new Font("Lucida Bright", Font.PLAIN, 16));
-		data.setBounds(216, 183, 54, 19);
+		data.setBounds(202, 183, 54, 19);
 		contentPane.add(data);
-		
+
 		txtObs = new JTextField();
-		txtObs.setBounds(139, 280, 205, 20);
+		txtObs.setBounds(138, 294, 205, 20);
 		contentPane.add(txtObs);
 		txtObs.setColumns(10);
 		txtNomeCliente.setBounds(166, 130, 210, 23);
 		contentPane.add(txtNomeCliente);
 		txtNomeCliente.setColumns(10);
-		
-				JLabel lblTraco = new JLabel("New label");
-				lblTraco.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/Sem t\u00EDtulo.png")));
-				lblTraco.setBounds(399, 108, 2, 268);
-				contentPane.add(lblTraco);
+
+		JLabel lblTraco = new JLabel("New label");
+		lblTraco.setIcon(new ImageIcon(HotelEN.class.getResource("/imagens/Sem t\u00EDtulo.png")));
+		lblTraco.setBounds(399, 108, 2, 268);
+		contentPane.add(lblTraco);
 
 		JLabel lblAte = new JLabel("to");
-		lblAte.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
-		lblAte.setBounds(280, 182, 36, 20);
+		lblAte.setFont(new Font("Lucida Bright", Font.PLAIN, 15));
+		lblAte.setBounds(273, 182, 36, 20);
 		contentPane.add(lblAte);
-		
-				JLabel lblCuidadores = new JLabel("Caretakers");
-				lblCuidadores.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
-				lblCuidadores.setBounds(461, 108, 117, 20);
-				contentPane.add(lblCuidadores);
 
-		JLabel lblData = new JLabel("Date:");
+		JLabel lblCuidadores = new JLabel("Caretakers");
+		lblCuidadores.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
+		lblCuidadores.setBounds(461, 108, 117, 20);
+		contentPane.add(lblCuidadores);
+
+		JLabel lblData = new JLabel("Date: ");
 		lblData.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
 		lblData.setBounds(97, 182, 63, 20);
 		contentPane.add(lblData);
 
-		JLabel lblName = new JLabel("Name:");
-		lblName.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
-		lblName.setBounds(97, 131, 73, 20);
-		contentPane.add(lblName);
-		
+		JLabel label = new JLabel("Name:");
+		label.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
+		label.setBounds(97, 131, 73, 20);
+		contentPane.add(label);
+
 		JLabel lblAlgumaObservao = new JLabel("Observations");
 		lblAlgumaObservao.setFont(new Font("Lucida Bright", Font.PLAIN, 18));
-		lblAlgumaObservao.setBounds(187, 253, 140, 28);
+		lblAlgumaObservao.setBounds(183, 268, 126, 28);
 		contentPane.add(lblAlgumaObservao);
-		
-		
 
 		JLabel lblCuidador = new JLabel("Hotel IPet");
 		lblCuidador.setFont(new Font("Bauhaus 93", Font.PLAIN, 50));
 		lblCuidador.setBounds(231, 37, 247, 65);
 		contentPane.add(lblCuidador);
-		
+
 		JLabel lblTipo = new JLabel("Animal:");
 		lblTipo.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
-		lblTipo.setBounds(97, 222, 89, 23);
+		lblTipo.setBounds(97, 224, 81, 23);
 		contentPane.add(lblTipo);
-		
+
 		JLabel lblDe = new JLabel("from");
-		lblDe.setFont(new Font("Lucida Bright", Font.PLAIN, 20));
-		lblDe.setBounds(156, 177, 61, 31);
+		lblDe.setFont(new Font("Lucida Bright", Font.PLAIN, 15));
+		lblDe.setBounds(157, 176, 66, 31);
 		contentPane.add(lblDe);
 
 		JLabel background = new JLabel("");
