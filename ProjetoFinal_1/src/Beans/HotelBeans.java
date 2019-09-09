c6ff2da0aa9b5d864b1866fb7565fce904d929db
Jordana Tomio <dannatomio@gmail.com>
2019-09-09 17:17:20 -0300

    Update HotelBeans.java

--- ProjetoFinal_1/src/Beans/HotelBeans.java
+++ ProjetoFinal_1/src/Beans/HotelBeans.java
@@ -5,15 +5,22 @@ public class HotelBeans {
 	private String nome;
 	private String descricao;
 	private String nascimento;
+	private String dataComeco;
+	private String dataFinal;
+	private String cuidador;
 
 	public HotelBeans() {
 	}
 
-	public HotelBeans(String nome, String descricao, String nascimento) {
+	public HotelBeans(String nome, String descricao, String nascimento, String cuidador, String dataComeco,
+			String dataFinal) {
 		super();
 		this.nome = nome;
 		this.descricao = descricao;
 		this.nascimento = nascimento;
+		this.cuidador = cuidador;
+		this.dataComeco = dataComeco;
+		this.dataFinal = dataFinal;
 	}
 
 	public String getNome() {
@@ -39,5 +46,28 @@ public class HotelBeans {
 	public void setNascimento(String nascimento) {
 		this.nascimento = nascimento;
 	}
-	
+
+	public String getDataComeco() {
+		return dataComeco;
+	}
+
+	public void setDataComeco(String dataComeco) {
+		this.dataComeco = dataComeco;
+	}
+
+	public String getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(String dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+	public String getCuidador() {
+		return cuidador;
+	}
+
+	public void setCuidador(String cuidador) {
+		this.cuidador = cuidador;
+	}
 }
